b6da64bf887991c7ec8a013767ba8d65904b0fa0
Ben Rousch <brousch@gmail.com>
2013-08-23 19:00:12 -0400

    cycler

--- waznexserver/static/js/main.js
+++ waznexserver/static/js/main.js
@@ -14,3 +14,5 @@ function confirm_delete(event) {
         event.preventDefault();
     }
 }
+
+function Cycle() { if(typeof this.currentCell == "undefined") { this.currentCell = 0; } var allCells = document.getElementsByClassName("cell"); for(var c = 0; c < allCells.length; ++c) { allCells[c].style.WebkitTransform = (this.currentCell == c ? "scale(2)" : null); } ++this.currentCell; if(this.currentCell >= allCells.length) { this.currentCell = 0; } } setInterval(Cycle, 2500);
